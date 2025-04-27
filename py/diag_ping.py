66b96751a9f2bce9f7826f85647c78cde3ab4004
SUKEsann2000 <SUKEsann2000@users.noreply.github.com>
2025-04-27 19:18:47 +0900

    診断機能の出力処理を統一: check_ping関数内でのprintAndAppend呼び出しをdefsモジュールを通じて行うように変更し、出力の一貫性を向上させました。

--- py/diag_ping.py
+++ py/diag_ping.py
@@ -1,35 +1,33 @@
+import defs
+
 from ping3 import ping
 
 # PING
 
-output = []
 
 def check_ping(urls):
+    output = []
 
-    printAndAppend("************************")
-    printAndAppend("Start pinging")
-    printAndAppend("")
+    output = defs.printAndAppend("************************", output)
+    output = defs.printAndAppend("Start pinging",output)
+    output = defs.printAndAppend("",output)
     
     max_len = max(len(url) for url in urls)
 
-    printAndAppend(f"{'Status':<10} {'URL':<{max_len}} {'Time':>10}")
-    printAndAppend("-" * (10 + max_len + 10))
+    output = defs.printAndAppend(f"{'Status':<10} {'URL':<{max_len}} {'Time':>10}",output)
+    output = defs.printAndAppend("-" * (10 + max_len + 10),output)
 
     for url in urls:
         res = ping(url)
         if res is not None and res is not False:
-            printAndAppend(f"{'Success':<10} {url.ljust(max_len)} {res:>10.4f}")
+            output = defs.printAndAppend(f"{'Success':<10} {url.ljust(max_len)} {res:>10.4f}",output)
         else:
-            error_message = f"Failed to ping {url}"
-            printAndAppend(f"{'Failed':<10} {url.ljust(max_len)} {'-':>10}")
+            output.append(f"Failed to ping {url}")
+            output = defs.printAndAppend(f"{'Failed':<10} {url.ljust(max_len)} {'-':>10}",output)
             return "\n".join(output), False
 
-    printAndAppend("")
-    printAndAppend("finished pinging")
-    printAndAppend("************************")
+    output = defs.printAndAppend("",output)
+    output = defs.printAndAppend("finished pinging",output)
+    output = defs.printAndAppend("************************",output)
 
     return "\n".join(output), True
-
-def printAndAppend(message):
-    print(message)
-    output.append(message)
