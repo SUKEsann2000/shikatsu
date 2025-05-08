de9435a086ab6d68df3e06bb585c7c85c825716a
SUKEsann2000 <129367282+SUKEsann2000@users.noreply.github.com>
2025-05-08 20:44:18 +0900

    diag_speedtest.pyで、サーバーをIPAに固定

--- py/diag_speedtest.py
+++ py/diag_speedtest.py
@@ -2,7 +2,7 @@ import sys
 import speedtest
 
 def get_speed_test():
-    servers = []
+    servers = [14623] # IPAのサーバーを選択
     stest = speedtest.Speedtest(secure=True)
     stest.get_servers(servers)
     best = stest.get_best_server()
