354ab40d77fca9c6b196ab9f7ce99928481c2381
SUKEsann2000 <129367282+SUKEsann2000@users.noreply.github.com>
2025-05-11 11:32:21 +0900

    diag_speedtest.pyにsecure=Trueを追加

--- py/diag_speedtest.py
+++ py/diag_speedtest.py
@@ -3,7 +3,7 @@ import speedtest
 
 def get_speed_test():
     servers = [48463] # IPAのサーバーを選択
-    stest = speedtest.Speedtest()
+    stest = speedtest.Speedtest(secure=True)
     stest.get_servers(servers)
     best = stest.get_best_server()
     return stest, best
